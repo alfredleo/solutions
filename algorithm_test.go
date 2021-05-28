5d9e586cf58a69910c8c91f03094383fc3b202d0
Alfred <alfredleo@gmail.com>
2021-05-28 15:22:02 +0500

    feature: import one commit from another repo
    cd <path_A>
    git checkout <branch_A>
    git fetch <path_B> <ref_that_contains_commitB>
    git cherry-pick <commitB>
    
    git fetch C:\WORK\Golang-learn\p1\ozon.ru 3fb1bdfda7d3f08c2d2d20131ca8ecb9500bd37d
    git cherry-pick 3fb1bdfda7d3f08c2d2d20131ca8ecb9500bd37d

---
+++ algorithm_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func maxOnesAfterRemoveItem(arr []byte) uint {
+
+	return 0
+}
+
+func TestSomething(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(maxOnesAfterRemoveItem([]byte{0, 0}) == 0)
+	assert.True(maxOnesAfterRemoveItem([]byte{0, 1}) == 1)
+	assert.True(maxOnesAfterRemoveItem([]byte{1, 0}) == 1)
+	assert.True(maxOnesAfterRemoveItem([]byte{1, 1}) == 1)
+	assert.True(maxOnesAfterRemoveItem([]byte{1, 1, 0, 1, 1}) == 4)
+	assert.True(maxOnesAfterRemoveItem([]byte{1, 1, 0, 1, 1, 0, 1, 1, 1}) == 5)
+	assert.True(maxOnesAfterRemoveItem([]byte{1, 1, 0, 1, 1, 0, 1, 1, 1, 0}) == 5)
+}
+
+func TestIsPalindromeLoopWithPalindromeText(t *testing.T) {
+
+	//assert(maxOnesAfterRemoveItem([0,0]) == 0)
+	//assert(maxOnesAfterRemoveItem[0,1] == 1)
+	//assert(maxOnesAfterRemoveItem[1,0] == 1)
+	//assert(maxOnesAfterRemoveItem[1,1] == 1)
+	//assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1] == 4)
+	//assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1, 0, 1, 1, 1] == 5)
+	//assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1, 0, 1, 1, 1, 0] == 5)
+}
+
+/*
+I Задача о длиной цепочке единиц ⭐⭐
+
+Дана последоватльность 0 и 1
+Нужно найти саммую длинную последовательность из 1 (единиц) после удаления любого элемента
+
+func maxOnesAfterRemoveItem([]byte) uint
+assert(maxOnesAfterRemoveItem[0,0] == 0)
+assert(maxOnesAfterRemoveItem[0,1] == 1)
+assert(maxOnesAfterRemoveItem[1,0] == 1)
+assert(maxOnesAfterRemoveItem[1,1] == 1)
+assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1] == 4)
+assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1, 0, 1, 1, 1] == 5)
+assert(maxOnesAfterRemoveItem[1, 1, 0, 1, 1, 0, 1, 1, 1, 0] == 5)
+Что хочется увидеть:
+
+Алгоритм со сложностью O(N) по времени и O(1) по памяти
+*/
